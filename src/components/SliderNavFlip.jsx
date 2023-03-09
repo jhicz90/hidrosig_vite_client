665a3df5eb46b7c50c278fcce49c0ca091d630c3
jhicz90 <jhicz90@gmail.com>
2023-03-08 23:11:11 -0500

    SLIDERFLIP

--- src/components/SliderNavFlip.jsx
+++ src/components/SliderNavFlip.jsx
@@ -1,11 +1,11 @@
+import styled from 'styled-components'
 import Flicking from '@egjs/react-flicking'
 
 import '@egjs/react-flicking/dist/flicking.css'
-import styled from 'styled-components'
 
 export const SliderNavFlip = ({ children }) => {
     return (
-        <FlipWrapper moveType='freeScroll' align='prev'>
+        <FlipWrapper moveType='freeScroll' bound={true} align='prev'>
             {children}
         </FlipWrapper>
     )
