4afcc3a4a05548864e803acfd5714bb354a64f37
jhicz90 <jose.hans.gg@gmail.com>
2023-08-26 19:31:53 -0500

    TEST FIX IN PRODUCTION

--- src/modules/userregister/components/AreaFarmInformation.jsx
+++ src/modules/userregister/components/AreaFarmInformation.jsx
@@ -1,17 +1,24 @@
-import { useEffect } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { Controller, useForm } from 'react-hook-form'
 import { Button, Col, Form, Row } from 'react-bootstrap'
+import Select from 'react-select'
 import AsyncSelect from 'react-select/async'
-import { AsyncSelectCustomBlockByJunta, Liner, OptionLocation } from '../../../components'
-import { searchLocation, useGetFarmByIdQuery, useUpdateFarmByIdMutation } from '../../../store/actions'
+import { Liner, OptionBlock, OptionLocation, OptionOrgz } from '../../../components'
+import { searchJunta, searchLocation, useGetFarmByIdQuery, useLazyGetListBlockQuery, useUpdateFarmByIdMutation } from '../../../store/actions'
+import { useAuthStore } from '../../../hooks'
 
 export const AreaFarmInformation = () => {
 
     const { prpid } = useParams()
+    const { lvlAccess } = useAuthStore()
     const { data = null } = useGetFarmByIdQuery(prpid)
     const [updateFarm, { isLoading: isUpdating }] = useUpdateFarmByIdMutation()
     const { control, register, handleSubmit, watch, setValue, getValues, reset } = useForm()
+
+    const [searchBlock] = useLazyGetListBlockQuery()
+    const [optionsCommittee, setOptionsCommittee] = useState(watch('junta')?.committees || [])
+    const [optionsBlock, setOptionsBlock] = useState(watch('committee')?.blocks || [])
 
     const handleUpdate = (updateData) => {
         updateFarm({
@@ -20,11 +27,52 @@ export const AreaFarmInformation = () => {
         })
     }
 
+    const customFilterCommittee = useCallback((candidate, input) => {
+        if (input.length > 0) {
+            return (
+                candidate.data.name.toLowerCase().includes(input.toLowerCase()) ||
+                candidate.data.nameAbrev.toLowerCase().includes(input.toLowerCase()) ||
+                candidate.data.nameLarge.toLowerCase().includes(input.toLowerCase()) ||
+                candidate.data.nameLargeAbrev.toLowerCase().includes(input.toLowerCase()) ||
+                candidate.data.docid.toLowerCase().includes(input.toLowerCase()) ||
+                candidate.data.email.toLowerCase().includes(input.toLowerCase())
+            )
+        }
+        return true
+    }, [])
+
+    const customFilterBlock = useCallback((candidate, input) => {
+        if (input.length > 0) {
+            return (
+                candidate.data.name.toLowerCase().includes(input.toLowerCase()) ||
+                candidate.data.code.toLowerCase().includes(input.toLowerCase()) ||
+                candidate.data.desc.toLowerCase().includes(input.toLowerCase())
+            )
+        }
+        return true
+    }, [])
+
     useEffect(() => {
         reset({
             ...data
         })
     }, [reset, data])
+
+    useEffect(() => {
+        if (Object.values(watch()).length > 0) {
+            if (watch('junta').hasOwnProperty('committees')) {
+                setOptionsCommittee(watch('junta')?.committees || [])
+            } else {
+                setOptionsCommittee([])
+            }
+
+            if (watch('committee').hasOwnProperty('blocks')) {
+                setOptionsBlock(watch('committee')?.blocks || [])
+            } else {
+                setOptionsBlock([])
+            }
+        }
+    }, [watch])
 
     return (
         <form id='form-userregister-areafarm-edit-info' onSubmit={handleSubmit(handleUpdate)}>
@@ -184,11 +232,196 @@ export const AreaFarmInformation = () => {
             </Row>
             <Liner>Bloque de riego y ubicación</Liner>
             <Row>
-                <AsyncSelectCustomBlockByJunta
+                {/* <AsyncSelectCustomBlockByJunta
                     control={control}
                     setValue={setValue}
                     watch={watch}
-                />
+                /> */}
+                {
+                    lvlAccess === 1
+                        ?
+                        <React.Fragment>
+                            <Col md={6}>
+                                <Form.Group as={Row} className='mb-3'>
+                                    <Form.Label column md={4}>
+                                        Junta de usuarios
+                                    </Form.Label>
+                                    <Col md={8}>
+                                        <Controller
+                                            name='junta'
+                                            control={control}
+                                            rules={{
+                                                required: true,
+                                                onChange: ({ target }) => {
+                                                    if (target.value?.committees?.length > 0) {
+                                                        setOptionsCommittee(target.value.committees)
+                                                    } else {
+                                                        setOptionsCommittee([])
+                                                    }
+                                                    // setOptionsBlock([])
+                                                    setValue('committee', null)
+                                                    setValue('block', null)
+                                                }
+                                            }}
+                                            render={
+                                                ({ field }) =>
+                                                    <AsyncSelect
+                                                        {...field}
+                                                        classNamePrefix='rc-select'
+                                                        styles={{
+                                                            control: (baseStyles, state) => ({
+                                                                ...baseStyles,
+                                                                minHeight: '90px',
+                                                            }),
+                                                        }}
+                                                        isClearable
+                                                        defaultOptions
+                                                        loadOptions={searchJunta}
+                                                        hideSelectedOptions
+                                                        menuPlacement='auto'
+                                                        menuPosition='absolute'
+                                                        placeholder={`Buscar...`}
+                                                        loadingMessage={({ inputValue }) => `Buscando '${inputValue}'`}
+                                                        noOptionsMessage={({ inputValue }) => `Sin resultados con ...${inputValue}`}
+                                                        getOptionValue={e => e._id}
+                                                        getOptionLabel={e => <OptionOrgz orgz={e} />}
+                                                    />
+                                            }
+                                        />
+                                    </Col>
+                                </Form.Group>
+                            </Col>
+                            <Col md={6}>
+                                <Form.Group as={Row} className='mb-3'>
+                                    <Form.Label column md={4}>
+                                        Comisión de usuarios
+                                    </Form.Label>
+                                    <Col md={8}>
+                                        <Controller
+                                            name='committee'
+                                            control={control}
+                                            rules={{
+                                                required: true,
+                                                onChange: ({ target }) => {
+                                                    if (target.value?.blocks?.length > 0) {
+                                                        setOptionsBlock(target.value.blocks)
+                                                    } else {
+                                                        setOptionsBlock([])
+                                                    }
+                                                    setValue('block', null)
+                                                }
+                                            }}
+                                            render={
+                                                ({ field }) =>
+                                                    <Select
+                                                        {...field}
+                                                        classNamePrefix='rc-select'
+                                                        styles={{
+                                                            control: (baseStyles, state) => ({
+                                                                ...baseStyles,
+                                                                minHeight: '90px',
+                                                            }),
+                                                        }}
+                                                        isClearable
+                                                        isDisabled={watch('junta') === null || optionsCommittee.length === 0}
+                                                        options={optionsCommittee}
+                                                        filterOption={customFilterCommittee}
+                                                        hideSelectedOptions
+                                                        menuPlacement='auto'
+                                                        menuPosition='absolute'
+                                                        placeholder={`Buscar...`}
+                                                        loadingMessage={({ inputValue }) => `Buscando '${inputValue}'`}
+                                                        noOptionsMessage={({ inputValue }) => `Sin resultados con ...${inputValue}`}
+                                                        getOptionValue={e => e._id}
+                                                        getOptionLabel={e => <OptionOrgz orgz={e} />}
+                                                    />
+                                            }
+                                        />
+                                    </Col>
+                                </Form.Group>
+                            </Col>
+                            <Col md={6}>
+                                <Form.Group as={Row} className='mb-3'>
+                                    <Form.Label column md={4}>
+                                        Bloque de riego
+                                    </Form.Label>
+                                    <Col md={8}>
+                                        <Controller
+                                            name='block'
+                                            control={control}
+                                            rules={{ required: true }}
+                                            render={
+                                                ({ field }) =>
+                                                    <Select
+                                                        {...field}
+                                                        classNamePrefix='rc-select'
+                                                        styles={{
+                                                            control: (baseStyles, state) => ({
+                                                                ...baseStyles,
+                                                                minHeight: '90px',
+                                                            }),
+                                                        }}
+                                                        isClearable
+                                                        isDisabled={watch('junta') === null || optionsBlock.length === 0}
+                                                        options={optionsBlock}
+                                                        filterOption={customFilterBlock}
+                                                        hideSelectedOptions
+                                                        menuPlacement='auto'
+                                                        menuPosition='absolute'
+                                                        placeholder={`Buscar...`}
+                                                        loadingMessage={({ inputValue }) => `Buscando '${inputValue}'`}
+                                                        noOptionsMessage={({ inputValue }) => `Sin resultados con ...${inputValue}`}
+                                                        getOptionValue={e => e._id}
+                                                        getOptionLabel={e => <OptionBlock block={e} />}
+                                                    />
+                                            }
+                                        />
+                                    </Col>
+                                </Form.Group>
+                            </Col>
+                        </React.Fragment>
+                        :
+                        <React.Fragment>
+                            <Col md={6}>
+                                <Form.Group as={Row} className='mb-3'>
+                                    <Form.Label column md={4}>
+                                        Bloque de riego
+                                    </Form.Label>
+                                    <Col md={8}>
+                                        <Controller
+                                            name='block'
+                                            control={control}
+                                            rules={{ required: true }}
+                                            render={
+                                                ({ field }) =>
+                                                    <AsyncSelect
+                                                        {...field}
+                                                        classNamePrefix='rc-select'
+                                                        styles={{
+                                                            control: (baseStyles, state) => ({
+                                                                ...baseStyles,
+                                                                minHeight: '90px',
+                                                            }),
+                                                        }}
+                                                        isClearable
+                                                        loadOptions={async (e) => {
+                                                            return (await searchBlock(e)).data
+                                                        }}
+                                                        menuPlacement='auto'
+                                                        menuPosition='fixed'
+                                                        placeholder={`Buscar...`}
+                                                        loadingMessage={({ inputValue }) => `Buscando '${inputValue}'`}
+                                                        noOptionsMessage={({ inputValue }) => `Sin resultados con ...${inputValue}`}
+                                                        getOptionValue={e => e._id}
+                                                        getOptionLabel={e => <OptionBlock block={e} />}
+                                                    />
+                                            }
+                                        />
+                                    </Col>
+                                </Form.Group>
+                            </Col>
+                        </React.Fragment>
+                }
                 <Col md={6}>
                     <Form.Group as={Row} className='mb-3'>
                         <Form.Label column md={4}>
